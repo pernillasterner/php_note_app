e588f7fdd25c596387055023e0297ab25534face
Pernilla Sterner <psterner@hotmail.com>
2024-11-15 10:51:06 +0100

    Display All Notes from User with id 1 on the Note Page

--- controllers/notes.php
+++ controllers/notes.php
@@ -9,6 +9,5 @@ $heading = 'Notes';
 
 $notes = $db->query('select * from notes where user_id = 1')->fetchAll();
 
-dd($notes);
 
 require "views/notes.view.php";
--- views/notes.view.php
+++ views/notes.view.php
@@ -4,8 +4,9 @@
 <?php require('partials/banner.php') ?>
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-
-        <p>Now you are on the Notes Page</p>
+        <?php foreach ($notes as $note) : ?>
+            <li><?= $note['body'] ?></li>
+        <?php endforeach; ?>
     </div>
 </main>
 <?php require('partials/footer.php') ?>
