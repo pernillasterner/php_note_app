3a3fcce2d1c805a7f87e6f3befc291bac56e5104
Pernilla Sterner <psterner@hotmail.com>
2024-11-15 14:42:12 +0100

    Create function to handle auth of user

--- functions.php
+++ functions.php
@@ -15,3 +15,12 @@ function urlIs($value)
 {
     return $_SERVER['REQUEST_URI'] === $value;
 }
+
+// Make it possible to overwrite status code
+function authorize($condition, $status = Response::FORBIDDEN)
+{
+    // Status code 403 - Forbidden
+    if (! $condition) {
+        abort($status);
+    }
+}
