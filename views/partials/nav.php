8c6998414c5cf575abd978aca01378763a4599ad
Pernilla Sterner <psterner@hotmail.com>
2024-11-15 10:01:15 +0100

    Change avatar

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -30,7 +30,7 @@
                             <button type="button" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                                 <span class="absolute -inset-1.5"></span>
                                 <span class="sr-only">Open user menu</span>
-                                <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                                <img class="h-8 w-8 rounded-full" src="https://avatarfiles.alphacoders.com/374/374586.png" alt="">
                             </button>
                         </div>
 
@@ -84,7 +84,7 @@
         <div class="border-t border-gray-700 pb-3 pt-4">
             <div class="flex items-center px-5">
                 <div class="shrink-0">
-                    <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                    <img class="h-10 w-10 rounded-full" src="https://avatarfiles.alphacoders.com/374/374586.png" alt="">
                 </div>
                 <div class="ml-3">
                     <div class="text-base/5 font-medium text-white">Tom Cook</div>
