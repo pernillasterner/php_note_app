46b90fcf1711a21b06e9f39ee0bce72a1dddb5dc
Pernilla Sterner <psterner@hotmail.com>
2024-11-15 11:30:41 +0100

    Clean up

--- controllers/note.php
+++ controllers/note.php
@@ -8,9 +8,7 @@ $db = new Database($config['database']);
 $heading = 'My Note';
 
 
-// 1. Get the id from the url
-// $id = $_GET['id']; !!.....If the variable is only used once, inline it....!!!
-// 2. Use that id to get the corresponding data from the database. Use wildcard to prevent sql injections
+// Get id from the url and use that id to get the corresponding data from the database. Use wildcard to prevent sql injections.
 $notes = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->fetchAll();
 
 
