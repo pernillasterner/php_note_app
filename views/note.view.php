9449d834a5de8f657512952ddb2ff6111c7c7d20
Pernilla Sterner <psterner@hotmail.com>
2024-11-15 12:01:10 +0100

    Add go back link

--- views/note.view.php
+++ views/note.view.php
@@ -5,6 +5,10 @@
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
 
+        <p class="mb-6">
+            <a href="/notes" class="text-blue-500 underline">go back...</a>
+        </p>
+
         <p><?= $note['body'] ?></p>
 
     </div>
