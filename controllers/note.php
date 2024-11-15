a86fe837ae550215df818929fe6af01d3f664dcf
Pernilla Sterner <psterner@hotmail.com>
2024-11-15 11:28:20 +0100

    Use wildcard to prevent sql injection. Make variable inline if only used once

--- controllers/note.php
+++ controllers/note.php
@@ -9,9 +9,9 @@ $heading = 'My Note';
 
 
 // 1. Get the id from the url
-$id = $_GET['id'];
-// 2. Use that id to get the corresponding data from the database
-$note = $db->query("select * from notes where user_id = $id")->fetch();
+// $id = $_GET['id']; !!.....If the variable is only used once, inline it....!!!
+// 2. Use that id to get the corresponding data from the database. Use wildcard to prevent sql injections
+$notes = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->fetchAll();
 
 
 require "views/note.view.php";
