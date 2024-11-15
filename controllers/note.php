61d1bb5a4b3ad3d24bfad230bf59babeea41b64c
Pernilla Sterner <psterner@hotmail.com>
2024-11-15 14:43:40 +0100

    Call auth function to check status of current user

--- controllers/note.php
+++ controllers/note.php
@@ -13,25 +13,13 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 $heading = 'Note';
+$currentUserId = 1;
 
 
 // Get id from the url and use that id to get the corresponding data from the database. Use wildcard to prevent sql injections.
-$note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->fetch();
+$note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->findOrFail();
 
-// Two reasons why we want to abort
-// 1. If the note exists in the database but current user did not create is. Access problem
-// 2. Note doesn't exist in the database.
-// Status code 404 - Page Not Found
-if (! $note) {
-    abort();
-}
-
-$currentUserId = 1;
-
-// Status code 403 - Forbidden
-if ($note['user_id'] !== $currentUserId) {
-    abort(Response::FORBIDDEN);
-}
+authorize($note['user_id'] !== $currentUserId);
 
 
 require "views/note.view.php";
