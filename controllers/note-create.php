f38e87adaa8e25aae684080becefae955b8c0f2d
Pernilla Sterner <psterner@hotmail.com>
2024-11-15 16:24:19 +0100

    Add htmlspecialchars to form

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,10 +1,21 @@
 <?php
 
+// Config data
+$config = require('config.php');
+// Initialize a new instance of a class
+$db = new Database($config['database']);
+
 $heading = 'Create a Note';
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // dd($_POST);
+    // Insert the incoming respons to the database. 
+    // Need to add the useID
+
+    $db->query('INSERT INTO notes (body, user_id) VALUES(:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => 1
+    ]);
 }
 
 
