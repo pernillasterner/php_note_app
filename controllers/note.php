61e5a98b3b4fbe5169e63aff7ad9c61bc8e254d0
Pernilla Sterner <psterner@hotmail.com>
2024-11-15 13:58:00 +0100

    Create a class to handle status response. Use CONSTANS when Page Not Found or when User Not Authorized

--- controllers/note.php
+++ controllers/note.php
@@ -8,6 +8,7 @@
 // Config data
 $config = require('config.php');
 
+
 // Initialize a new instance of a class
 $db = new Database($config['database']);
 
@@ -17,16 +18,19 @@ $heading = 'Note';
 // Get id from the url and use that id to get the corresponding data from the database. Use wildcard to prevent sql injections.
 $note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->fetch();
 
-// Two resons why we want to abort
+// Two reasons why we want to abort
 // 1. If the note exists in the database but current user did not create is. Access problem
 // 2. Note doesn't exist in the database.
 // Status code 404 - Page Not Found
 if (! $note) {
     abort();
 }
+
+$currentUserId = 1;
+
 // Status code 403 - Forbidden
-if ($note['user_id'] !== 1) {
-    abort(403);
+if ($note['user_id'] !== $currentUserId) {
+    abort(Response::FORBIDDEN);
 }
 
 
