21c36bcaa7d69583bdc9297ae19ccceb54381848
Pernilla Sterner <psterner@hotmail.com>
2024-11-16 16:19:07 +0100

    Add description to explain the code

--- controllers/notes.php
+++ controllers/notes.php
@@ -1,13 +1,25 @@
 <?php
-// Config data
+
+/**
+ * Controller: Display Notes
+ * 
+ * This script handles the retrieval of notes for a specific user
+ * from the db and prepares the data for rendering in the
+ * corresponding view file (`notes.view.php`)
+ * 
+ */
+
+// Loads configuration data
 $config = require('config.php');
 
-// Initialize a new instance of a class
+// Connects to the database
 $db = new Database($config['database']);
 
 $heading = 'My Notes';
 
+// Retrievs notes for user_id = 1
 $notes = $db->query('select * from notes where user_id = 1')->get();
 
 
+// Passes data to the view for rendering
 require "views/notes.view.php";
