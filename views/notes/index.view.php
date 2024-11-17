661de20beac6e9b07d2e1992c70ce495d74a9c90
Pernilla Sterner <psterner@hotmail.com>
2024-11-17 15:32:24 +0100

    Update paths using _DIR_

--- views/notes/index.view.php
+++ views/notes/index.view.php
@@ -1,14 +1,23 @@
-<?php require('partials/head.php') ?>
-<?php require('partials/nav.php') ?>
+<?php require(__DIR__ . '/../partials/head.php') ?>
+<?php require(__DIR__ . '/../partials/nav.php') ?>
+<?php require(__DIR__ . '/../partials/banner.php') ?>
 
-<?php require('partials/banner.php') ?>
+
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <ul>
+            <?php foreach ($notes as $note) : ?>
+                <li>
+                    <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
+                        <?= htmlspecialchars($note['body']); ?>
+                    </a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
 
-        <p class="mb-6">
-            <a href="/notes" class="text-blue-500 underline">go back...</a>
+        <p class="mt-6">
+            <a href="/notes/create" class="text-blue-500 hover:underline">Create Note</a>
         </p>
-
     </div>
 </main>
-<?php require('partials/footer.php') ?>
+<?php require(__DIR__ . '/../partials/footer.php') ?>
