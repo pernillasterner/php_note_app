1af3503738748ead7102662f63e53ce0da7263f4
Pernilla Sterner <psterner@hotmail.com>
2024-11-20 18:44:27 +0100

    Expand router to handle multiple request types

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -4,39 +4,8 @@
  * Controller: Creating Notes
  */
 
-use Core\Database;
-use Core\Validator;
-
-require base_path('Core/Validator.php');
-
-// Config data
-$config = require base_path('config.php');
-// Initialize a new instance of a class
-$db = new Database($config['database']);
-
-$errors = [];
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-    // Check if POST contains string. :: Calling a static method on a Class
-    if (! Validator::string($_POST['body'], 1, 1000)) {
-        $errors['body'] = 'A body of no more than 1,000 characters is required';
-    }
-
-
-    // If no validation errors proceed
-    if (empty($errors)) {
-        // Insert the incoming response to the database. 
-        // Need to add the useID
-        $db->query('INSERT INTO notes (body, user_id) VALUES(:body, :user_id)', [
-            'body' => $_POST['body'],
-            'user_id' => 1
-        ]);
-    }
-}
-
 
 view("notes/create.view.php", [
     'heading' => 'Create Note',
-    'errors' => $errors
+    'errors' => []
 ]);
--- controllers/notes/store.php
+++ controllers/notes/store.php
@@ -1,4 +1,39 @@
 <?php
 
+/**
+ * Controller: Store a Specific Note
+ */
 
-dd('HELLO');
+use Core\Validator;
+use Core\Database;
+
+
+// Config data
+$config = require base_path('config.php');
+// Initialize a new instance of a class
+$db = new Database($config['database']);
+
+// Check if POST contains string. :: Calling a static method on a Class
+if (! Validator::string($_POST['body'], 1, 1000)) {
+    $errors['body'] = 'A body of no more than 1,000 characters is required';
+}
+
+// If validation errors
+if (! empty($errors)) {
+    return view("notes/create.view.php", [
+        'heading' => 'Create Note',
+        'errors' => $errors
+    ]);
+}
+
+
+
+// Insert the incoming response to the database. 
+// Need to add the useID
+$db->query('INSERT INTO notes (body, user_id) VALUES(:body, :user_id)', [
+    'body' => $_POST['body'],
+    'user_id' => 1
+]);
+
+header('location: /notes');
+die();
