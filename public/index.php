c1b873c4b21b954dfae14c27152eae606fcac3e8
Pernilla Sterner <psterner@hotmail.com>
2024-11-20 18:52:36 +0100

    Require bootstrap file

--- public/index.php
+++ public/index.php
@@ -17,6 +17,8 @@ spl_autoload_register(function ($class) {
     require base_path("{$class}.php");
 });
 
+require base_path('bootstrap.php');
+
 // require base_path('Core/router.php');
 $router = new \Core\Router();
 
