b5921c66d8f543b0da38e6bd248f5271ac94fe46
Pernilla Sterner <psterner@hotmail.com>
2024-11-20 12:19:34 +0100

    Add logic to DELETE Notes

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -13,21 +13,44 @@ use Core\Database;
 // Loads configuration data
 $config = require base_path('config.php');
 
-
 // Connects to the database
 $db = new Database($config['database']);
 
 $currentUserId = 1;
 
+// You submitted the page, you want to take this pathway
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-// Fetches the note using the provided ID with parameterized queries to prevent SQL injection.
-$note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->findOrFail();
+    // Fetches the note using the provided ID with parameterized queries to prevent SQL injection.
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
 
-// Ensures that the current user is authorized to view the note.
-authorize($note['user_id'] === $currentUserId);
+    // Ensures that the current user is authorized to view the note.
+    authorize($note['user_id'] === $currentUserId);
 
-// Passes data to the view for rendering
-view("notes/show.view.php", [
-    'heading' => 'Note',
-    'note' => $note
-]);
+    // form was submitted. delete the current note.
+    $db->query('delete from notes where id = :id', [
+        'id' => $_GET['id']
+    ]);
+
+    // Redirect user to notes page
+    header('location: /notes');
+    exit();
+} else {
+
+
+    // Fetches the note using the provided ID with parameterized queries to prevent SQL injection.
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
+
+    // Ensures that the current user is authorized to view the note.
+    authorize($note['user_id'] === $currentUserId);
+
+    // Passes data to the view for rendering
+    view("notes/show.view.php", [
+        'heading' => 'Note',
+        'note' => $note
+    ]);
+}
