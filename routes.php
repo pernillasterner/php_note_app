41cf268406f231a6882b6fa1ec40f4fd983ca2ae
Pernilla Sterner <psterner@hotmail.com>
2024-11-21 13:51:19 +0100

    Add route to handle note updates

--- routes.php
+++ routes.php
@@ -17,6 +17,7 @@ $router->get('/note', 'controllers/notes/show.php');
 $router->delete('/note', 'controllers/notes/destroy.php');
 
 $router->get('/note/edit', 'controllers/notes/edit.php');
+$router->patch('/note', 'controllers/notes/update.php');
 
 
 $router->get('/notes/create', 'controllers/notes/create.php');
