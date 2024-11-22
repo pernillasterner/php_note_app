6c5df77fd5a0f45dc65e6ccae0616a74417096d1
Pernilla Sterner <psterner@hotmail.com>
2024-11-22 10:29:16 +0100

    Add logic to show when user is logged in or not.

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -28,22 +28,16 @@
                     <div class="relative ml-3">
                         <div>
                             <button type="button" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                <span class="absolute -inset-1.5"></span>
+                                <!-- <span class="absolute -inset-1.5"></span> -->
                                 <span class="sr-only">Open user menu</span>
-                                <img class="h-8 w-8 rounded-full" src="https://avatarfiles.alphacoders.com/374/374586.png" alt="">
+                                <?php if ($_SESSION['user'] ?? false) : ?>
+                                    <img class="h-8 w-8 rounded-full" src="https://avatarfiles.alphacoders.com/374/374586.png" alt="Avatar image">
+                                <?php else : ?>
+                                    <a href="/register" class="text-white">Register</a>
+                                <?php endif; ?>
                             </button>
                         </div>
 
-                        <!--
-                Dropdown menu, show/hide based on menu state.
-
-                Entering: "transition ease-out duration-100"
-                  From: "transform opacity-0 scale-95"
-                  To: "transform opacity-100 scale-100"
-                Leaving: "transition ease-in duration-75"
-                  From: "transform opacity-100 scale-100"
-                  To: "transform opacity-0 scale-95"
-              -->
                         <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                             <!-- Active: "bg-gray-100 outline-none", Not Active: "" -->
                             <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
@@ -84,11 +78,15 @@
         <div class="border-t border-gray-700 pb-3 pt-4">
             <div class="flex items-center px-5">
                 <div class="shrink-0">
-                    <img class="h-10 w-10 rounded-full" src="https://avatarfiles.alphacoders.com/374/374586.png" alt="">
+                    <?php if ($_SESSION['user'] ?? false) : ?>
+                        <img class="h-10 w-10 rounded-full" src="https://avatarfiles.alphacoders.com/374/374586.png" alt="Avatar image">
+                    <?php else : ?>
+                        <a href="/register" class="text-white">Register</a>
+                    <?php endif; ?>
                 </div>
                 <div class="ml-3">
-                    <div class="text-base/5 font-medium text-white">Tom Cook</div>
-                    <div class="text-sm font-medium text-gray-400">tom@example.com</div>
+                    <div class="text-base/5 font-medium text-white">Pernilla</div>
+                    <div class="text-sm font-medium text-gray-400">pernilla@undeco.se</div>
                 </div>
                 <button type="button" class="relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                     <span class="absolute -inset-1.5"></span>
