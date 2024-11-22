0d691feba38402e07c9901ccfe26aa3a0b86680e
Pernilla Sterner <psterner@hotmail.com>
2024-11-22 15:36:32 +0100

    Handle if no middleware key is found

--- Core/Middleware/Middleware.php
+++ Core/Middleware/Middleware.php
@@ -6,8 +6,30 @@ namespace Core\Middleware;
 class Middleware
 {
 
-    const MAP = [
+    public const MAP = [
         'guest' => Guest::class,
         'auth' => Auth::class
     ];
+
+
+    public static function resolve($key)
+    {
+        // Check if key exists (auth, guest)
+        if (! $key) {
+            return;
+        }
+
+        // Instantiate the Middleware class and call the handle method
+        // Find the corresponding middleware class
+        $middleware = static::MAP[$key] ?? false; // If no key, set middleware to false
+
+
+        // if handle is null
+        if (! $middleware) {
+            throw new \Exception("No matching middleware found for key '{$key}'");
+            // Returns: Fatal error: Uncaught Exception: No matching middleware found for key 'foobar' in ...
+        }
+
+        (new $middleware)->handle();
+    }
 }
