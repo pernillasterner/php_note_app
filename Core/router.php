33b1a773d15da2d989321c785f0e2d5dc8061dac
Pernilla Sterner <psterner@hotmail.com>
2024-11-22 13:15:42 +0100

    Add Middleware to Routers

--- Core/router.php
+++ Core/router.php
@@ -9,47 +9,78 @@ class Router
     public function add($method, $uri, $controller)
     {
         // From routes.php $router->get('/', 'controllers/index.php');
-        // $this->routes[] = [
-        //     'uri' => $uri, // '/'
-        //     'controller' => $controller, // 'controllers/index.php'
-        //     'method' => $method
-        // ];
+        $this->routes[] = [
+            'uri' => $uri, // '/'
+            'controller' => $controller, // 'controllers/index.php'
+            'method' => $method,
+            'middleware' => null
+        ];
 
         // Compact creates new associative array where the keys are the name of the given variable ($method, $uri, $controller)
-        $this->routes[] = compact('method', 'uri', 'controller');
+        // $this->routes[] = compact('method', 'uri', 'controller');
+
+        return $this;
     }
 
 
     public function get($uri, $controller)
     {
-        $this->add('GET', $uri, $controller);
+        return $this->add('GET', $uri, $controller);
     }
 
     public function post($uri, $controller)
     {
-        $this->add('POST', $uri, $controller);
+        return $this->add('POST', $uri, $controller);
     }
 
     public function delete($uri, $controller)
     {
-        $this->add('DELETE', $uri, $controller);
+        return $this->add('DELETE', $uri, $controller);
     }
 
     public function patch($uri, $controller)
     {
-        $this->add('PATCH', $uri, $controller);
+        return $this->add('PATCH', $uri, $controller);
     }
 
     public function put($uri, $controller)
     {
-        $this->add('PUT', $uri, $controller);
+        return $this->add('PUT', $uri, $controller);
     }
 
+    public function only($key)
+    {
+        // Apply middleware ex 'guest' to the router 
+        $this->routes[array_key_last($this->routes)]['middleware'] = $key;
+
+        return $this;
+    }
+
+    // when we try to match a route, does the uri and method match up? If, so apply middleware
     public function route($uri, $method)
     {
 
         foreach ($this->routes as $route) {
+            // dd($this->routes);
             if ($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
+
+                // apply the middleware
+                if ($route['middleware'] === 'guest') {
+                    if ($_SESSION['user'] ?? false) {
+                        // redirect users that are not signed in
+                        header('location: /');
+                        exit();
+                    }
+                }
+
+                if ($route['middleware'] === 'auth') {
+                    if (! $_SESSION['user'] ?? false) {
+                        // redirect users that are not signed in
+                        header('location: /');
+                        exit();
+                    }
+                }
+
                 return require base_path($route['controller']);
             }
         }
--- routes.php
+++ routes.php
@@ -12,7 +12,7 @@ $router->get('/about', 'controllers/about.php');
 $router->get('/contact', 'controllers/contact.php');
 
 
-$router->get('/notes', 'controllers/notes/index.php');
+$router->get('/notes', 'controllers/notes/index.php')->only('auth');
 $router->get('/note', 'controllers/notes/show.php');
 $router->delete('/note', 'controllers/notes/destroy.php');
 
@@ -23,6 +23,6 @@ $router->patch('/note', 'controllers/notes/update.php');
 $router->get('/notes/create', 'controllers/notes/create.php');
 $router->post('/notes', 'controllers/notes/store.php');
 
-
-$router->get('/register', 'controllers/registration/create.php');
+// /register should be restricted to only guests, not signed in users
+$router->get('/register', 'controllers/registration/create.php')->only('guest');
 $router->post('/register', 'controllers/registration/store.php');
