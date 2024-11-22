50fd76412b6a0861cb886465855683c55a682527
Pernilla Sterner <psterner@hotmail.com>
2024-11-22 09:40:44 +0100

    Add query to check if registered user exists in the db. Add user if not

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -4,13 +4,60 @@
  * Controller: Stores Email and Password
  */
 
+use Core\App;
+use Core\Database;
+use Core\Validator;
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 // validate form inputs
+$errors = [];
+
+if (! Validator::email($email)) {
+    $errors['email'] = 'Please provide a valid email address.';
+}
+
+if (! Validator::string($password, 2, 255)) {
+    $errors['password'] = 'Please provide a password of at least seven characters.';
+}
+
+if (! empty($errors)) {
+    return view('registration/create.view.php', [
+        'errors' => $errors
+    ]);
+}
 
 
+// connect to database
+$db = App::resolve(Database::class);
 // check if the account already exists
-    // If yes, redirect to a login page
-    // If not, save to the database, and then log the user in, and redirect
+$user = $db->query('select * from users where email = :email', [
+    'email' => $email
+])->find();
+
+
+
+if ($user) {
+    // then someone with that email already exists and has an account
+    // If yes, redirect to a login page.
+    header('location: /');
+} else {
+    // If not, save one to the database, and then log the user in, and redirect.
+
+    // save to database
+    $db->query(
+        'INSERT INTO users(email, password) VALUES(:email, :password)',
+        [
+            'email' => $email,
+            'password' => $password
+        ]
+    );
+
+    // log in user
+
+
+
+    // redirect
+
+}
