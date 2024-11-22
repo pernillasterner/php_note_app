952d42dbf59a5009fefbd997714979e816e6d462
Pernilla Sterner <psterner@hotmail.com>
2024-11-22 15:54:31 +0100

    Use SESSION data to display the name of current user

--- views/index.view.php
+++ views/index.view.php
@@ -5,7 +5,9 @@
 <?php require('partials/banner.php') ?>
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <p>Now you are on the Home page</p>
+        <p>
+            Hello, <?= $_SESSION['user']['email'] ?? 'Guest' ?>.
+            Welcome to the homepage</p>
     </div>
 </main>
 <?php require('partials/footer.php') ?>
