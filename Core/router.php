fb32ab613813dec5f1077f7b434c3b4d1a775eee
Pernilla Sterner <psterner@hotmail.com>
2024-11-24 12:02:05 +0100

    Organize file structure

--- Core/router.php
+++ Core/router.php
@@ -71,7 +71,7 @@ class Router
                     Middleware::resolve($route['middleware']);
                 }
 
-                return require base_path($route['controller']);
+                return require base_path('Http/controllers/' . $route['controller']);
             }
         }
 
