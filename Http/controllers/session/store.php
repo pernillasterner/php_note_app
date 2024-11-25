e4121485eb2d5cb4974dadbf70180a1ac08e7932
Pernilla Sterner <psterner@hotmail.com>
2024-11-25 10:15:35 +0100

    Clean up code and refactoring some code snippets regarding login, logout, auth, validation

--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -5,46 +5,36 @@
  */
 
 
-use Core\App;
-use Core\Database;
+
+use Core\Authenticator;
 use Http\Forms\LoginForm;
 
-// connect to database
-$db = App::resolve(Database::class);
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-// validate form inputs
+// create a form
 $form = new LoginForm();
 
+// validate email and password
 if (! $form->validate($email, $password)) {
+    // if validation fails, return to login form and display the errors
     return view('session/create.view.php', [
         'errors' => $form->errors()
     ]);
 }
 
-
-// match the credentials
-$user = $db->query('select * from users where email = :email', [
-    'email' => $email
-])->find();
+// create a authenticator
+$auth = new Authenticator();
 
 
-if ($user) {
-    // we have a user, but we don´t know if the password provided matches what we have in the database.
-    if (password_verify($password, $user['password'])) {
-        login([
-            'email' => $email
-        ]);
-
-        header('location: /');
-        exit();
-    }
+// authenticate user based on email and password
+if ($auth->attempt($email, $password)) {
+    // sign in if success
+    redirect('/');
 }
 
-
-
+// if auth fails, return to login form and display the error message
 return view('session/create.view.php', [
     'errors' => [
         'email' => 'No matching account found for that email address and password'
