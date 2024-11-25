a7bbb7f4a8d2e1c85a90d439d80183cf441c7bf2
Pernilla Sterner <psterner@hotmail.com>
2024-11-25 17:26:08 +0100

    Install composer autoloader

--- public/index.php
+++ public/index.php
@@ -3,11 +3,15 @@
 use Core\Session;
 use Core\ValidationException;
 
-session_start();
 
 //  __DIR__ = Current directory
 // BASE_PATH = Root of the project
 const BASE_PATH = __DIR__ . '/../';
+
+// Automatically loads class files by converting the class name into a file path and requiring it.
+require BASE_PATH . '/vendor/autoload.php';
+
+session_start();
 
 // ...php_note_app/public/../
 // var_dump(BASE_PATH);
@@ -15,15 +19,8 @@ const BASE_PATH = __DIR__ . '/../';
 // Loading functions
 require BASE_PATH . 'Core/functions.php';
 
-
-// Automatically loads class files by converting the class name into a file path and requiring it.
-spl_autoload_register(function ($class) {
-    // $class = Core\Database
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    require base_path("{$class}.php");
-});
-
 require base_path('bootstrap.php');
+
 
 // require base_path('Core/router.php');
 $router = new \Core\Router();
